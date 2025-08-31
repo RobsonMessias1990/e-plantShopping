de3c7654a676feed8a54b931d9b72aec428160e4
RobsonMessias1990 <robson.messias1990@gmail.com>
2025-08-31 10:09:13 -0300

    atualização

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,18 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+  const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+// Find the item in the cart that matches the given name
+  const itemToUpdate = state.items.find(item => item.name === name);
+  if (itemToUpdate) {
+  itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+  }
+  else {
+    // If item does not exist, add it to the cart with quantity 1
+    state.items.push({ name, image, cost, quantity: 1 });
+    state.items = state.items.filter(item => item.name !== action.payload);
+  }
+},
   },
 });
 
